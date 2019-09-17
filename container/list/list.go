a826cb3facdaa7df69bc1e4957d42f1115326896
pradeep <pradeep.raja@gmail.com>
2019-09-17 22:39:31 +0530

    basic linked list implementation

---
+++ container/list/list.go
@@ -0,0 +1,86 @@
+// Package list implements double linked list
+package list
+
+// Item is an element of a linked list.
+type Item struct {
+	Value      interface{}
+	next, prev *Item
+}
+
+// Prev returns previous item
+func (e *Item) Prev() *Item {
+	return e.prev
+}
+
+// Next returns the next item
+func (e *Item) Next() *Item {
+	return e.next
+}
+
+// List represents a doubly linked list.
+type List struct {
+	first *Item
+	last  *Item
+	len   int
+}
+
+// New returns new list
+func New() *List {
+	return &List{first: nil}
+}
+
+// First return first item in the list
+func (l *List) First() *Item {
+	return l.first
+}
+
+// Last returns last item in array
+func (l *List) Last() *Item {
+	return l.last
+}
+
+// Length returns length
+func (l *List) Length() int {
+	return l.len
+}
+
+// Append new value to the list
+func (l *List) Append(v interface{}) {
+	item := &Item{Value: v, next: nil, prev: l.last}
+	if l.last == nil {
+		l.last, l.first = item, item
+	} else {
+		l.last.next = item
+		l.last = item
+	}
+	l.len++
+}
+
+// Prepend value to the list
+func (l *List) Prepend(v interface{}) {
+	item := &Item{Value: v, next: l.first, prev: nil}
+	if l.first == nil {
+		l.last, l.first = item, item
+	} else {
+		l.first.prev = item
+		l.first = item
+	}
+	l.len++
+}
+
+// Reverse the list O(n)
+func (l *List) Reverse() {
+	// Go to each item from last and swap prev and next
+	reverse := l.Last()
+	for i := 0; i < l.Length(); i++ {
+		c := reverse.prev
+		reverse.next, reverse.prev = reverse.prev, reverse.next
+		reverse = c
+	}
+	// Make first is last and last is first
+	l.first, l.last = l.last, l.first
+}
+
+func swap(i1, i2 *Item) {
+	i1.Value, i2.Value = i2.Value, i1.Value
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module pradeeplabs.com/algorithm
+
+go 1.12
